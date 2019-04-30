d1e662c7e4d683a08e59ce147058467864752362
mateusgpontes <mateus.gpontes@hotmail.com>
2019-04-30 13:52:52 -0300

    add parar-urso logic

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ function andaImg (image, file1, file2, file3) {
     else if(image.src == file3){
         image.src = file2
     }
+    setTimeout(() => {
+        image.src = file1
+    }, 700);
 } 
 
 const img = query("img");
@@ -32,37 +35,33 @@ let anda = 10;
 document.addEventListener("keypress", (andar) => {
     if(andar.code == "KeyW"){        
         img.style.marginBottom = anda + "px";
-        andaImg(img,"file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
+        andaImg(img, "file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-2.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-3.png"
         );
     }
     else if(andar.code == "KeyD"){
         img.style.marginLeft = anda + "px";
-        andaImg(img,"file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
+        andaImg(img, "file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-2.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-3.png"
         );
     }
     else if(andar.code == "KeyS"){
         img.style.marginTop = anda + "px";
-        andaImg(img,"file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
+        andaImg(img, "file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-2.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-3.png"
         );
     }
     else if(andar.code == "KeyA"){
         img.style.marginRight = anda + "px";
-        andaImg(img,"file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
+        andaImg(img, "file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-2.png",
         "file:///home/mateus/Documents/estudos/javaScript/anima/chara-3.png"
         );
     }else{
         img.src = "file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png";
-        andaImg(img,"file:///home/mateus/Documents/estudos/javaScript/anima/chara-1.png",
-        "file:///home/mateus/Documents/estudos/javaScript/anima/chara-2.png",
-        "file:///home/mateus/Documents/estudos/javaScript/anima/chara-3.png"
-        );
     }
 
     anda = anda + 15;
